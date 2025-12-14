b39e35cb821da399cf00bd091c9f216640d8a311
Adrians <adriansuparto_2226250027@mhs.mdp.ac.id>
2025-12-14 19:04:07 +0700

    Done implementasi prinsip SOLID

--- SOLID-code/src/Classes/SquareGeometry.java
+++
@@ -1,7 +0,0 @@
-package Classes;
-
-public class SquareGeometry extends RectangleGeometry {
-    public SquareGeometry(int x, int y, int size) {
-        super(x, y, x + size, y + size);
-    }
-}
--- SOLID-code/src/Main/Main.java
+++
@@ -1,9 +0,0 @@
-package Main;
-
-import View.RefactoredGUI;
-
-public class Main {
-    public static void main(String[] args) {
-        RefactoredGUI.main(args);
-    }
-}
